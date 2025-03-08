8a7235e7a0140ff327a83fa170f1ba47d0a2b151
yourmom64209 <2jes1366@gmail.com>
2025-03-08 02:01:04 -0800

    Update cocaine.js

--- cocaine.js
+++ cocaine.js
@@ -1,8 +1,18 @@
 elements.liquid_cocaine = {
 	color:"#FFFFFF",
-	behavior: behaviors.liquid,
+	behavior: behaviors.LIQUIDS,
 	category: "drugs",
 	state: "liquid",
 	stateLow:"cocaine",
 	tempLow: 0,
 }
+
+elements.cocaine = {
+	color:"#FFFFFF",
+	behavior: behaviors.POWDERS,
+	category: "drugs",
+	state: "powder",
+	stateHigh:"liquid_cocaine",
+	tempHigh: 100,
+}
+
